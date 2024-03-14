08a4292a4190723a648243eded2586d94c6e9ebd
Pedro Rosado <rondaplay@hotmail.com>
2024-03-14 16:12:22 +0100

    14/03/2024

--- Prueba_GitHub/src/Main.java
+++ Prueba_GitHub/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola soy peri");
+		System.out.println("Hola soy Peri");
 
 	}
 
---
+++ Prueba_GitHub/src/Models/Persona.java
@@ -0,0 +1,115 @@
+package Models;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellidos;
+
+    /**
+     * 
+     */
+    private int edad;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellidos 
+     * @param edad
+     */
+    public void Persona(String dni, String nombre, String apellidos, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * Modifica el DNI de la persona
+     * @param dni
+     */
+    public void setDni(String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve el DNI de la persona
+     * @return
+     */
+    public String getDni() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica el nombre
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve el nombre
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica los apellidos
+     * @param apellidos
+     */
+    public void setApellidos(String apellidos) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve los apellidos
+     * @return
+     */
+    public String getApellidos() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica la edad
+     * @param edad
+     */
+    public void setEdad(String edad) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve la edad
+     * @return
+     */
+    public int getEdad() {
+        // TODO implement here
+        return 0;
+    }
+
+}
