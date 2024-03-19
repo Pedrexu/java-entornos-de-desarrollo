570ecbcdf8699b7b0064f9b18a8de13029ee43ec
Pedro Rosado <rondaplay@hotmail.com>
2024-03-19 17:17:07 +0100

    Subido de nuevo

--- Figuras/src/modelos/Rectangulo.java
+++ Figuras/src/modelos/Rectangulo.java
@@ -12,59 +12,38 @@ public class Rectangulo {
     	this.altura = 1;
     }
 
-    /**
-     * @param base: int 
-     * @param altura: int
-     */
+   
     public Rectangulo(int base, int altura) {
     	this.base = base;
     	this.altura = altura;
     }
 
-    /**
-     * 
-     */
-
-    /**
-     * @param base: int
-     */
+    
     public int getBase() {
     	return this.base;
     }
 
-    /**
-     * @param base: int
-     */
+    
     public void setBase(int base) {
     	this.base = base;
     }
 
-    /**
-     * @param altura: int
-     */
+    
     public int getAltura() {
     	return this.altura;
     }
 
-    /**
-     * @param altura: int
-     */
+    
     public void setAltura(int altura) {
     	this.altura = altura;
     }
 
-    /**
-     * @param base: int 
-     * @param altura: int
-     */
+    
     public int area() {
         return base * altura;
     }
 
-    /**
-     * @param base: int 
-     * @param altura: int
-     */
+    
     public int perimetro() {
         return 2*this.base + 2*this.altura;
     }
